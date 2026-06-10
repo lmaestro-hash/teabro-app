eae4c47d1f940b0a146978b57441ab79526ecf58
lmaestro-hash <lmaestro86@gmail.com>
2026-06-10 13:25:03 +0300

    v2.6 keepalive fix

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ async function statEvent(action, uid) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
+      keepalive: true,
     });
   } catch {}
 }
